5cf1f97a6fb44264ea6957185033bed3156e9a79
marouane batrone <batrone.marouane@gmail.com>
2026-05-02 16:30:23 +0100

    add logger utility for structured logging

---
+++ utils/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+def get_logger(name: str) -> logging.Logger:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+        datefmt="%H:%M:%S",
+    )
+    return logging.getLogger(name)
